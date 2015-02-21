119bcc2d26e7bca4e9f1f8c6bda1a44a4f790854
Maciej Kazirod <Maciej.Kazirod@swisscom.com>
2015-02-21 13:19:23 +0100

    initial API sketch

--- server/server.py
+++ server/server.py
@@ -1,21 +1,43 @@
+#!flask/bin/python
+import json
+import sys
 import os
-try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
-except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
+import logging
+from flask import Flask, request
 
-# Read port selected by the cloud for our application
-PORT = int(os.getenv('VCAP_APP_PORT', 8000))
-# Change current directory to avoid exposure of control files
-os.chdir('static')
+app = Flask(__name__)
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
 
-httpd = Server(("", PORT), Handler)
-try:
-  print("Start serving at port %i" % PORT)
-  httpd.serve_forever()
-except KeyboardInterrupt:
-  pass
-httpd.server_close()
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
+logger = logging.getLogger(__name__)
 
+
+@app.route('/')
+def index():
+    logger.warning("hit!")
+    return "Hello, World!"
+
+
+@app.route('/measurements', methods=['GET'])
+def list_measurements():
+    logger.debug(os.environ["VCAP_SERVICES"])
+    j = json.loads(os.environ["VCAP_SERVICES"])
+    return "Nothing here"
+
+
+@app.route('/measurements', methods=['POST'])
+def add_measurement():
+    content = request.get_json(force=True)
+    logger.debug(content)
+    return "Written!"
+
+
+if __name__ == '__main__':
+    # Read port selected by the cloud for our application
+    PORT = int(os.getenv('VCAP_APP_PORT', 8000))
+    app.run(host='0.0.0.0', debug=True, port=PORT)
