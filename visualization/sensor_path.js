7f955fe74994b0fcacb8e7c5bcf5dd88077c1bcc
younes <benyounes.moumni@epfl.ch>
2015-02-22 12:27:34 +0100

    last version

--- visualization/sensor_path.js
+++ visualization/sensor_path.js
@@ -3,38 +3,133 @@ window.onload = function() {
         cartodb.createVis('map', vizjson);
 
 /////// Chart /////////////////////////////////////////////////////////////////////////
+$("#showTemperatureChart").on("click", function(evt){
+      $("#chartSelector .btn").removeClass("btn-info").addClass("btn-default");
+     $(evt.target).addClass("btn-info");
+     requestData("temperature", function(result){
+         pollutionCallback(result, "Temperature", "°");        
+     });
+});
 
-        var ctx = document.getElementById("chart").getContext("2d");
-		ctx.canvas.width  = window.innerWidth;
+$("#showPollutionChart").on("click", function(evt){
+     $("#chartSelector .btn").removeClass("btn-info").addClass("btn-default");
+     $(evt.target).addClass("btn-info");
+     requestData("pollution", function(result){
+         pollutionCallback(result, "Pollution", "PM");
+     });
+});
+    
+$('#showHumidityChart').on("click", function(evt){
+     $("#chartSelector .btn").removeClass("btn-info").addClass("btn-default");
+     $(evt.target).addClass("btn-info");
+     requestData("humidity", function(result){
+         pollutionCallback(result, "Humidity", "%");
+     });
+});
+$("#showNoiseChart").on("click", function(evt){
+     $("#chartSelector .btn").removeClass("btn-info").addClass("btn-default");
+     $(evt.target).addClass("btn-info");
+     requestData("audio", function(result){
+         pollutionCallback(result, "Noise", "dB");
+     });
+});
+    $("#showTemperatureChart").trigger("click");
+     }
 
-		var data = {
-    		labels: ["January", "February", "March", "April", "May", "June", "July"],
-    		datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
+var requestData = function(type, callback){
+
+    var temperatureUrl = 'https://a7673287-7829-48d0-9f19-1e5427c6111f-bluemix.cloudant.com/measurements/_design/temperature/_view/new-view?include_docs=true';
+$.ajax({
+          type: "GET",
+          //url: "https://a7673287-7829-48d0-9f19-1e5427c6111f-bluemix.cloudant.com/measurements/_all_docs?include_docs=true",
+    
+          url:'https://a7673287-7829-48d0-9f19-1e5427c6111f-bluemix.cloudant.com/measurements/_design/temperature/_view/'+type+'?include_docs=true',
+          username: "a7673287-7829-48d0-9f19-1e5427c6111f-bluemix",
+            password: "77f13e7ce5da7d3439b3b149a190043961ebf44a3a3a11fb5a72e15f1b4601f7",
+          xhrFields: {
+            withCredentials: true
+          },
+          dataType: 'jsonp',
+          data: {
+           username: "a7673287-7829-48d0-9f19-1e5427c6111f-bluemix",
+            password: "77f13e7ce5da7d3439b3b149a190043961ebf44a3a3a11fb5a72e15f1b4601f7",
+          },
+          crossDomain: true,
+          success: callback
+        });
+}
+
+var plotting = function(data, yAxis, yAxisFormat){
+  $('#chart').highcharts({
+        chart: {
+                type: 'column'
+            },
+     
+        xAxis: {
+             type: 'datetime',
+            reversed: false,
+            title: {
+                enabled: true,
+                text: 'Time'
+            },
+            labels: {
+            format: '{value:%H:%M:%S}',
+            },
+            maxPadding: 0.05,
+            showLastLabel: true
         },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
-	};
+        yAxis: {
+            title: {
+                text: yAxis
+            },
+            labels: {
+                formatter: function () {
+                    return this.value + ' ' + yAxisFormat;
+                }
+            },
+            lineWidth: 2
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            headerFormat: '<b>{series.name}</b><br/>',
+            pointFormat: '{value:%H-%M-%S} : {point.y}' + yAxisFormat
+        },
+        plotOptions: {
+            series: {
+                    stacking: 'normal'
+                }
+        },
+        series: [{
+            name: yAxis,
+            data:data,
+            pointStart: Date.UTC(2013, 0, 1),
+            pointInterval: 24 * 36e5
+        }]
+    });
 
-	var options = null;
-	var myLineChart = new Chart(ctx).Line(data, options);
+}
 
-    }
+pollutionCallback = function(result, chartTitle, units){
+    var pollutionData = [];
+        var data = result.rows.map(
+          function(obj){
+            var val = obj.value.sensor_value;
+            var latt = obj.value.lat;// + (0.1 * Math.random());
+            var lon = obj.value.lon; //+ (0.1 * Math.random());
+            var t = obj.value.timestamp;
+            var timestamp=Date.parse(t)
+
+            pollutionData.push([t, val]);
+
+            
+        
+         }
+        );
+        plotting(pollutionData, chartTitle, units);
+      };
+          
+
+       
 
